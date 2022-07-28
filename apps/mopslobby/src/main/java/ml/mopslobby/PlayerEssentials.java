3c00c3c6b0f648e0f337380fcd8c0c83bad08c76
SirCat07 <46454817+SirCat07@users.noreply.github.com>
2022-07-28 20:03:52 +0300

    мяу

--- apps/mopslobby/src/main/java/ml/mopslobby/PlayerEssentials.java
+++ apps/mopslobby/src/main/java/ml/mopslobby/PlayerEssentials.java
@@ -27,48 +27,15 @@ public class PlayerEssentials extends Commands {
 	public boolean commandsExecutor(CommandSender sender, Command command, String label, String[] args, Plugin plugin) {
 		Player player = (Player) sender;
 		String commandName = command.getName().toLowerCase(Locale.ROOT);
-		if (commandName.equals("pog")) {
-			player.sendTitle(ChatColor.RED + "POGGERS", "", 1, 5, 20);
-			return true;
-		}
+
 		if (commandName.equals("hub") || commandName.equals("lobby") || commandName.equals("l")) {
-			ScoreboardManager manager = Bukkit.getScoreboardManager();
-			Scoreboard board = manager.getMainScoreboard();
-			Objective objective = board.getObjective("leave");
-			Score score = objective.getScore(player);
-			score.setScore(1);
+			player.sendMessage("тп в хаб");
 			return true;
 		}
 		if (commandName.equals("kit")) {
 			//player.openInventory(Dependencies.getKits());
 			player.sendMessage("киты не работают");
 			return true;
-		}
-		if (commandName.equals("whoiam")) {
-			Bukkit.getScheduler().runTaskLater(plugin, () -> {
-				player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 0);
-				player.sendTitle(ChatColor.RED + "5", "", 0, 0, 20);
-				Bukkit.getScheduler().runTaskLater(plugin, () -> {
-					player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 0.4F);
-					player.sendTitle(ChatColor.GOLD + "4", "", 0, 0, 20);
-					Bukkit.getScheduler().runTaskLater(plugin, () -> {
-						player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 0.8F);
-						player.sendTitle(ChatColor.YELLOW + "3", "", 0, 0, 20);
-						Bukkit.getScheduler().runTaskLater(plugin, () -> {
-							player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 1.2F);
-							player.sendTitle(ChatColor.GREEN + "2", "", 0, 0, 20);
-							Bukkit.getScheduler().runTaskLater(plugin, () -> {
-								player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 1.6F);
-								player.sendTitle(ChatColor.AQUA + "1", "", 0, 0, 20);
-								Bukkit.getScheduler().runTaskLater(plugin, () -> {
-									player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1, 2F);
-									player.sendTitle(ChatColor.BLUE + "YOU ARE:", ChatColor.BLUE + "АБОБИЙ", 0, 0, 60);
-								}, 20L);
-							}, 20L);
-						}, 20L);
-					}, 20L);
-				}, 20L);
-			}, 20L);
 		}
 		if (commandName.equals("calc")) {
 			double number1 = Double.parseDouble(args[0]);
