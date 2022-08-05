b41c6aac75b6efef6a981271e478453580bd30a9
InFTord <arkaz2008@gmail.com>
2022-08-05 20:57:42 +0500

    вырезал кит инфа

--- apps/mops-lobby/src/main/java/ml/mops/lobby/Plugin.java
+++ apps/mops-lobby/src/main/java/ml/mops/lobby/Plugin.java
@@ -46,46 +46,7 @@ public class Plugin extends JavaPlugin implements Listener, CommandExecutor {
             ItemMeta meta = item.getItemMeta();
             assert meta != null;
 
-            if (meta.getDisplayName().equals(ChatColor.RESET + "кто")) {
-                int i = 0;
 
-                while (i < 9) {
-                    Entity entity = player.getLocation().getWorld().spawnEntity(player.getLocation(), EntityType.FROG);
-                    Frog frog = (Frog) entity;
-
-                    entity.setGravity(false);
-
-                    entity.getLocation().setYaw(player.getLocation().getYaw());
-                    entity.getLocation().setPitch(player.getLocation().getPitch());
-
-                    int randomY = (int) (Math.random() * (1 - 3 + 1)) + 1;
-                    entity.teleport(entity.getLocation().add(0, randomY, 0));
-
-                    int frogVariant = (int) (Math.random() * (1 - 3 + 1)) + 1;
-                    Frog.Variant variant = Frog.Variant.COLD;
-                    switch (frogVariant) {
-                        case 1 -> variant = Frog.Variant.TEMPERATE;
-                        case 3 -> variant = Frog.Variant.WARM;
-                    }
-                    frog.setVariant(variant);
-
-                    Bukkit.getScheduler().runTaskLater(this, () -> {
-                        entity.teleport(entity.getLocation().add(0, -1000, 0));
-                    }, 180L);
-
-                    Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
-                        entity.teleport(((Frog) entity).getEyeLocation());
-
-                        for(Entity entity2 : entity.getNearbyEntities(3, 3, 3)) {
-                            if(entity2 instanceof Player && entity2 != player) {
-                                ((Player) entity2).damage(1);
-                            }
-                        }
-                    }, 0L, 0L);
-
-                    i++;
-                }
-            }
         }
 
         event.setCancelled(!player.getScoreboardTags().contains("admin"));
