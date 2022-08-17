b2073eabda0954bbebd127d052f1b4f3d7469d07
Bogdan Fediushin <bogdan.fediushin@Gmail.com>
2022-08-17 14:43:10 +0200

    аалофаофы\эва

--- apps/mops-lobby/src/main/java/ml/mops/lobby/Plugin.java
+++ apps/mops-lobby/src/main/java/ml/mops/lobby/Plugin.java
@@ -29,6 +29,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
