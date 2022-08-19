5ef19a81863c1783fe6ba83a40198222570140d2
SirCat07 <46454817+SirCat07@users.noreply.github.com>
2022-08-19 11:54:57 +0300

    кет детектер 2

--- apps/mops-lobby/src/main/java/ml/mops/lobby/Plugin.java
+++ apps/mops-lobby/src/main/java/ml/mops/lobby/Plugin.java
@@ -92,17 +92,17 @@ public class Plugin extends JavaPlugin implements Listener, CommandExecutor {
             event.setCancelled(true);
         }
 
-        try {
-            if (event.getAction().isRightClick()) {
-                if (player.getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.GOLD + "Cat Detector")) {
-                    if(player.getName().toLowerCase(Locale.ROOT).contains("cat")) {
-                        player.sendTitle("You are:", ChatColor.GREEN + "CAT", 10, 10, 10);
-                    } else {
-                        player.sendTitle("You are:", ChatColor.RED + "NOT CAT", 10, 10, 10);
-                    }
+        if (event.getAction().isRightClick()) {
+            System.out.println("кликнул");
+            if (player.getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.GOLD + "Cat Detector")) {
+                System.out.println("кет детектер");
+                if(player.getName().toLowerCase(Locale.ROOT).contains("cat")) {
+                    player.sendTitle("You are:", ChatColor.GREEN + "CAT", 10, 10, 10);
+                } else {
+                    player.sendTitle("You are:", ChatColor.RED + "NOT CAT", 10, 10, 10);
                 }
             }
-        } catch (Exception ignored) {}
+        }
     }
 
     @EventHandler
