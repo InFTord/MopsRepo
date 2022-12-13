4a98d8db1cc3fa008d7807450aff56ab33118c8c
SirCat07 <46454817+SirCat07@users.noreply.github.com>
2022-12-13 15:44:21 +0200

    а так жпжуж ну пожауста

--- libs/mops-base/src/main/java/ml/mops/base/maps/Map.java
+++ libs/mops-base/src/main/java/ml/mops/base/maps/Map.java
@@ -1,14 +1,13 @@
 package ml.mops.base.maps;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.plugin.Plugin;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -17,45 +16,44 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum Map {
-    DESERT("desert", ChatColor.YELLOW + "Desert", MapType.PVP, Material.SANDSTONE),
+    DESERT("https://cdn.discordapp.com/attachments/897853554340020254/1052219255392849920/desert.txt", ChatColor.YELLOW + "Desert", MapType.PVP, Material.SANDSTONE),
     PLAINS("desert", ChatColor.GREEN + "Plains", MapType.PVP, Material.GRASS_BLOCK),
     AQUA("desert", ChatColor.AQUA + "Aqua", MapType.PVP, Material.WARPED_WART_BLOCK),
     TAIGA("desert", ChatColor.DARK_GREEN + "Taiga", MapType.PVP, Material.SPRUCE_LEAVES),
     CITY("desert", ChatColor.GOLD + "City", MapType.PVP, Material.BRICKS),
     MOPSFORT("desert", ChatColor.RED + "MopsFort", MapType.PVP, Material.RED_TERRACOTTA);
 
-    final String fileName;
+    final String fileURL;
     final String name;
     final MapType mapType;
     final Material type;
 
     Map (String fileName, String name, MapType mapType, Material type) {
-        this.fileName = fileName;
+        this.fileURL = fileName;
         this.name = name;
         this.mapType = mapType;
         this.type = type;
     }
 
-    public String getFileName() {
-        return fileName;
+    public String getFileURL() {
+        return fileURL;
     }
 
-    public String[] getRowArray(Plugin plugin) {
+    public String[] getRowArray() {
         String[] rowArray = new String[] {""};
 
-        System.out.println(Paths.get("plugins/" + plugin.getName() + "/src/main/java/ml/mops/base/maps/" + mapType.getFilePath() + "/" + fileName + ".txt"));
-
         try {
-            List<String> stringList = Files.readAllLines(Paths.get("plugins/" + plugin.getName() + "/src/main/java/ml/mops/base/maps/" + mapType.getFilePath() + "/" + fileName + ".txt"), StandardCharsets.UTF_8);
-            System.out.println(stringList);
+            InputStream stream = new URL(fileURL).openStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream));
 
             StringBuilder stringBuilder = new StringBuilder();
 
-            for(String string : stringList) {
-                stringBuilder.append(string);
+            String inputLine;
+            while ((inputLine = bufferedReader.readLine()) != null) {
+                stringBuilder.append(inputLine);
+                stringBuilder.append(System.lineSeparator());
             }
-
-            System.out.println(stringBuilder);
+            bufferedReader.close();
 
             rowArray = stringBuilder.toString().split("\n");;
         } catch (IOException ignored) { }
--- libs/mops-base/src/main/java/ml/mops/base/maps/MapType.java
+++ libs/mops-base/src/main/java/ml/mops/base/maps/MapType.java
@@ -1,15 +1,5 @@
 package ml.mops.base.maps;
 
 public enum MapType {
-    PVP("pvp");
-
-    final String filePath;
-
-    MapType (String filePath) {
-        this.filePath = filePath;
-    }
-
-    public String getFilePath() {
-        return filePath;
-    }
+    PVP
 }
