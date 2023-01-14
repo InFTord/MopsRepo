fcf9c054101c59966349749b2fc46ac70e24b2da
SirCat07 <46454817+SirCat07@users.noreply.github.com>
2023-01-14 14:14:42 +0200

    чото начал

--- libs/mops-base/src/main/java/ml/mops/base/structures/MopsStructure.java
+++ libs/mops-base/src/main/java/ml/mops/base/structures/MopsStructure.java
@@ -1,5 +1,45 @@
 package ml.mops.base.structures;
 
-public class MopsStructure {
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+import org.bukkit.util.EulerAngle;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MopsStructure {
+    Location center = null;
+    List<MopsStructureBlock> blockList = new ArrayList<>();
+    HashMap<MopsStructureBlock, Location> offset = new HashMap<>();
+    HashMap<MopsStructureBlock, EulerAngle> angle = new HashMap<>();
+
+    World world = null;
+
+    List<ArmorStand> standList = new ArrayList<>();
+
+    MopsStructure(World thisWorld, Location thisCenter, List<MopsStructureBlock> thisBlockList, HashMap<MopsStructureBlock, Location> thisOffset, HashMap<MopsStructureBlock, EulerAngle> thisAngle) {
+        world = thisWorld;
+        center = thisCenter;
+        blockList = thisBlockList;
+        offset = thisOffset;
+        angle = thisAngle;
+    }
+
+    public void summonStructure() {
+        for(MopsStructureBlock block : blockList) {
+            ArmorStand stand = (ArmorStand) world.spawnEntity(center.add(offset.get(block)), EntityType.ARMOR_STAND);
+
+            stand.setHelmet(block.getBlock());
+            stand.setHeadPose(angle.get(block));
+
+            standList.add(stand);
+        }
+    }
+
+    public void rotate(int x, int y, int z) {
+
+    }
 }
--- libs/mops-base/src/main/java/ml/mops/base/structures/MopsStructureBlock.java
+++ libs/mops-base/src/main/java/ml/mops/base/structures/MopsStructureBlock.java
@@ -1,11 +1,26 @@
 package ml.mops.base.structures;
 
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.util.EulerAngle;
 
 public class MopsStructureBlock {
     public boolean enchanted = false;
-    public Material block = Material.DIAMOND_BLOCK;
+    public Material block = Material.AIR;
     public EulerAngle angle = new EulerAngle(0, 0, 0);
+
+    public ItemStack getBlock() {
+        ItemStack item = new ItemStack(block);
+        ItemMeta meta = item.getItemMeta();
+
+        meta.addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 1, true);
+
+        item.setItemMeta(meta);
+
+        return item;
+    }
 }
