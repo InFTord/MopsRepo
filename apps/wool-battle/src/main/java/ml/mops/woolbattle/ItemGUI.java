2d3edd14a5e98570be8586182dac6dc52c2e59c0
SirCat07 <46454817+SirCat07@users.noreply.github.com>
2023-01-28 20:40:24 +0200

    фикс

--- apps/wool-battle/src/main/java/ml/mops/woolbattle/ItemGUI.java
+++ apps/wool-battle/src/main/java/ml/mops/woolbattle/ItemGUI.java
@@ -31,24 +31,24 @@ public class ItemGUI implements Listener {
     public void setItems() {
         Items items = new Items(plugin);
 
-        inv.setItem(0, items.shears(plugin.lang));
-        inv.setItem(1, items.boomstickMK1(plugin.lang));
-        inv.setItem(2, items.boomstickMK2(plugin.lang));
-        inv.setItem(3, items.boomstickMK3(plugin.lang));
-        inv.setItem(4, items.slimeball(plugin.lang));
-        inv.setItem(5, items.platform(plugin.lang));
-        inv.setItem(6, items.platform3d(plugin.lang));
-        inv.setItem(7, items.doubleJumpBoots(plugin.lang, Teams.RED));
-        inv.setItem(8, items.doubleJumpBoots(plugin.lang, Teams.YELLOW));
-        inv.setItem(9, items.doubleJumpBoots(plugin.lang, Teams.GREEN));
-        inv.setItem(10, items.doubleJumpBoots(plugin.lang, Teams.BLUE));
-        inv.setItem(11, items.doubleJumpBoots(plugin.lang, Teams.SPECTATOR));
-        inv.setItem(12, items.leaves(plugin.lang));
-        inv.setItem(13, items.enderpearl(plugin.lang));
-        inv.setItem(14, items.shield(plugin.lang));
-        inv.setItem(15, items.axe(plugin.lang));
-        inv.setItem(16, items.stockBow(plugin.lang));
-        inv.setItem(17, items.potion(plugin.lang));
+        inv.setItem(0, items.shears("eng"));
+        inv.setItem(1, items.boomstickMK1("eng"));
+        inv.setItem(2, items.boomstickMK2("eng"));
+        inv.setItem(3, items.boomstickMK3("eng"));
+        inv.setItem(4, items.slimeball("eng"));
+        inv.setItem(5, items.platform("eng"));
+        inv.setItem(6, items.platform3d("eng"));
+        inv.setItem(7, items.doubleJumpBoots("eng", Teams.RED));
+        inv.setItem(8, items.doubleJumpBoots("eng", Teams.YELLOW));
+        inv.setItem(9, items.doubleJumpBoots("eng", Teams.GREEN));
+        inv.setItem(10, items.doubleJumpBoots("eng", Teams.BLUE));
+        inv.setItem(11, items.doubleJumpBoots("eng", Teams.SPECTATOR));
+        inv.setItem(12, items.leaves("eng"));
+        inv.setItem(13, items.enderpearl("eng"));
+        inv.setItem(14, items.shield("eng"));
+        inv.setItem(15, items.axe("eng"));
+        inv.setItem(16, items.stockBow("eng"));
+        inv.setItem(17, items.potion("eng"));
 
     }
 
